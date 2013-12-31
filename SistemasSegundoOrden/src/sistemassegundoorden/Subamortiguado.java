209cc09320b3f97e57ddba9090fda7846b4bcc14
Loli Delgado <madodela@gmail.com>
2013-12-30 21:15:36 -0600

    Respuesta subamortiguada corregida

--- SistemasSegundoOrden/src/sistemassegundoorden/Subamortiguado.java
+++ SistemasSegundoOrden/src/sistemassegundoorden/Subamortiguado.java
@@ -21,8 +21,8 @@ public class Subamortiguado {
 
     public double ftct(double t) {
         double ct1, ct2;
-        ct1 = 1 - (Math.exp(-chi * wn * t) / Math.sqrt(1 - chi * chi));
-        ct2 = wd * t + Math.tan(Math.sqrt(1 - chi * chi) / chi);
-        return ct1 * Math.sin(ct2);
+        ct1 = (Math.exp(-chi * wn * t) / Math.sqrt(1 - chi * chi));
+        ct2 = wd * t +(1/Math.tan(Math.sqrt(1 - chi * chi) / chi)) ;
+        return 1 - ct1 * Math.sin(ct2);
     }
 }
